3c0a1f54d157c57cfa973cb639dfcf9f1ddc2d09
Sam Schoedel <seschoedel@gmail.com>
2024-06-19 15:06:25 -0500

    Set up project

--- 2d-biped.jl
+++ 2d-biped.jl
@@ -7,3 +7,19 @@ render(vis)
 
 ##
 
+# Figure out dynamics (probably with Lagrangian dynamics?)
+# Maybe try figuring it out with Newtonian dynamics also
+
+# Simulate the dynamics with contact (might require solving
+# an optimization problem to ensure that the feet don't go
+# through the ground)
+
+# Write stabilizing controller (start with LQR) to balance
+# maybe start with feet fixed to ground with baumgarte 
+# stabilization for this
+
+# Write stabilizing controller that can handle constraints
+# friction cone constraints, joint limits, torque limits, etc.
+
+# Write controller that can track a walking trajectory
+# might require hybrid dynamics?
